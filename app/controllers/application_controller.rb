4d1062e695a0daabc95d45bbced64d0365a9f6dc
Carson Mulligan <103183546+carsonmulligan@users.noreply.github.com>
2024-12-30 14:15:37 -0600

    ✨ feat: Update browser support in ApplicationController to allow all browsers while maintaining security

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
+  # Allow all browsers while maintaining security
+  skip_before_action :verify_browser_support, if: -> { request.format.json? }
+  allow_browser versions: :modern, except: %w[bookmarks scholarships]
 end
