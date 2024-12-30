ca4ada53a6cf13561e1fac03332da3c3dec6cf96
Carson Mulligan <103183546+carsonmulligan@users.noreply.github.com>
2024-12-30 14:16:08 -0600

    🚑 fix: Remove skip_before_action and add skip_after_action for verifying browser support in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
   # Allow all browsers while maintaining security
-  skip_before_action :verify_browser_support, if: -> { request.format.json? }
   allow_browser versions: :modern, except: %w[bookmarks scholarships]
+  skip_after_action :verify_browser_support, if: -> { request.format.json? }
 end
