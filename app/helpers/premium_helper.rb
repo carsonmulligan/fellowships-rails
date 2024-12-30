34c77b25670e899a3b9e25cb35c55cc0a1a1de47
Carson Mulligan <103183546+carsonmulligan@users.noreply.github.com>
2024-12-30 00:23:28 -0600

    🚑 fix: adjust premium_user? method to only check if user is signed in

--- app/helpers/premium_helper.rb
+++ app/helpers/premium_helper.rb
@@ -1,10 +1,10 @@
 module PremiumHelper
   def premium_user?
-    user_signed_in? && current_user.premium?
+    user_signed_in?  # All logged-in users are premium users
   end
 
   def require_premium
-    unless premium_user?
+    unless user_signed_in?
       store_location_for(:user, request.fullpath)
       redirect_to root_path, alert: 'This feature requires a Fellows membership.'
     end
